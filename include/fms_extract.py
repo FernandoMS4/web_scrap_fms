8b25ceeba99cca760c62f590f5b1f1a81967cc89
FernandoMS4 <fernando381046@gmail.com>
2025-08-21 23:58:39 -0300

    dev: att headers

--- include/fms_extract.py
+++ include/fms_extract.py
@@ -8,18 +8,12 @@ from bs4 import BeautifulSoup as bs
 
 
 headers = {
-    "Host": "lista.mercadolivre.com.br",
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "pt-BR,en;q=0.5",
-    "Alt-Used": "lista.mercadolivre.com.br",
     "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Priority": "u=0, i"
 }
+
 
 def captura_produtos_mercado_livre(url: str,headers_= headers):
 
