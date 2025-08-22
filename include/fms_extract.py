962efc8a1681e7466cdfdf7d27440c0319f0852c
FernandoMS4 <fernando381046@gmail.com>
2025-08-22 00:02:03 -0300

    dev: att headers

--- include/fms_extract.py
+++ include/fms_extract.py
@@ -9,9 +9,7 @@ from bs4 import BeautifulSoup as bs
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "pt-BR,en;q=0.5",
-    "Connection": "keep-alive",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
 }
 
 
